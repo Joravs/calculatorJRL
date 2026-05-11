99bbc5616359a98d29d086388781d9eb3f4865ec
joravs <jordyl.reyesl@gmail.com>
2026-05-11 18:25:02 +0000

    Añadido Jenkinsfile Con CheckStyle

--- src/main/java/com/ciberJRL/calculatorJRL/Calculator.java
+++ src/main/java/com/ciberJRL/calculatorJRL/Calculator.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class Calculator {
-	int sum(int a, int b) {
-		return a + b;
-	}
+int sum(int a, int b) {
+return a + b;
+}
 }
--- src/main/java/com/ciberJRL/calculatorJRL/CalculatorJrlApplication.java
+++ src/main/java/com/ciberJRL/calculatorJRL/CalculatorJrlApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CalculatorJrlApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CalculatorJrlApplication.class, args);
-	}
-
+public static void main(String[] args) {
+SpringApplication.run(CalculatorJrlApplication.class, args);
+}
 }
